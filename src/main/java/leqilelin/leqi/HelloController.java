b18fed1ca09b1b0fabc84883e80990b08896b9a0
vincentzhang <vincentzhang@ea.com>
2019-09-12 16:23:16 +0800

    First commit

---
+++ src/main/java/leqilelin/leqi/HelloController.java
@@ -0,0 +1,12 @@
+package leqilelin.leqi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController{
+    @RequestMapping("/")
+    public String index(){
+        return "This is for LeqiLelin!";
+    }
+}
